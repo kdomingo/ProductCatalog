df7ee785f1fbd35ad54cbc5927d3699cbf3df019
Kyle Emmanuel <ke.domingz@gmail.com>
2026-09-10 18:22:49 +0800

    Adjustments: Product list ui

--- app/src/main/java/com/example/productcatalog/ui/screens/ProductList.kt
+++ app/src/main/java/com/example/productcatalog/ui/screens/ProductList.kt
@@ -4,49 +4,51 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.foundation.text.input.clearText
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import coil3.compose.AsyncImage
 import com.example.productcatalog.data.models.Product
 import com.example.productcatalog.ui.viewmodels.ProductListViewModel
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.debounce
 import org.koin.androidx.compose.koinViewModel
+import kotlin.time.Duration.Companion.milliseconds
 
+@OptIn(FlowPreview::class)
 @Composable
 fun ProductListScreen(
     modifier: Modifier = Modifier,
@@ -58,6 +60,7 @@ fun ProductListScreen(
 
     LaunchedEffect(searchFieldState) {
         snapshotFlow { searchFieldState.text }
+            .debounce(4000.milliseconds)
             .collect { text -> viewModel.search(text.toString()) }
     }
 
@@ -66,6 +69,9 @@ fun ProductListScreen(
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 state = searchFieldState,
+                placeholder = {
+                    Text("Search product")
+                },
                 trailingIcon = {
                     if (searchFieldState.text.isNotEmpty()) {
                         IconButton(onClick = {
@@ -75,6 +81,10 @@ fun ProductListScreen(
                         }
                     }
                 })
+
+            if (state.loading) {
+                LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+            }
 
             ProductList(products = state.products)
         }
@@ -87,9 +97,20 @@ private fun ProductList(
     products: List<Product>,
     onProductClicked: (Product) -> Unit = {}
 ) {
-    LazyColumn(modifier = modifier) {
-        items(items = products, key = { item -> item.id }) { product ->
-            ProductItemRow(product = product, onProductClicked = onProductClicked)
+    if (products.isNotEmpty()) {
+        LazyColumn(modifier = modifier) {
+            items(items = products, key = { item -> item.id }) { product ->
+                ProductItemRow(product = product, onProductClicked = onProductClicked)
+                HorizontalDivider()
+            }
+        }
+    } else {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("No Products Found")
         }
     }
 }
@@ -97,7 +118,11 @@ private fun ProductList(
 @Composable
 fun ProductTag(tag: String) {
     Card(colors = CardDefaults.cardColors(containerColor = Color.LightGray)) {
-        Text(tag, style = TextStyle(fontSize = 12.sp), modifier = Modifier.padding(4.dp))
+        Text(
+            tag,
+            style = TextStyle(fontSize = 12.sp),
+            modifier = Modifier.padding(horizontal = 6.dp, vertical = 4.dp)
+        )
     }
 }
 
@@ -106,43 +131,53 @@ private fun ProductItemRow(
     product: Product? = null,
     onProductClicked: (Product) -> Unit = {}
 ) {
+
+    val thumbnailPlaceholder = rememberVectorPainter(Icons.Filled.ShoppingCart)
+
     Card(
         modifier = Modifier
-            .height(150.dp)
+            .wrapContentHeight()
+            .fillMaxWidth()
             .clickable(enabled = null != product) {
                 onProductClicked(product!!)
             },
         shape = RoundedCornerShape(size = 4.dp),
         colors = CardDefaults.cardColors(containerColor = Color.White)
     ) {
-        Column(
-            modifier = Modifier
-                .padding(4.dp)
-                .fillMaxHeight(),
-            verticalArrangement = Arrangement.spacedBy(4.dp)
-        ) {
-            if (null != product && product.thumbnail.isNotEmpty()) {
+
+        Row {
+            product?.thumbnail?.takeIf { thumbnail -> thumbnail.isNotEmpty() }?.let { thumbnail ->
                 AsyncImage(
-                    model = product.thumbnail,
+                    model = thumbnail,
                     contentDescription = "product thumbnail",
-                    modifier = Modifier.fillMaxHeight(0.5f)
+                    placeholder = thumbnailPlaceholder,
+                    modifier = Modifier.weight(0.3f)
                 )
-            }
-            Text(product?.title ?: "Unknown")
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            } ?: Icon(Icons.Filled.ShoppingCart, contentDescription = "fallback product icon")
+
+            Column(
+                modifier = Modifier
+                    .padding(4.dp).weight(0.7f),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
             ) {
-                Icon(Icons.Filled.Star, contentDescription = "ratings star")
-                Text("${product?.rating ?: 0}")
-            }
-
-            LazyRow(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
-                items(product?.tags ?: emptyList(), key = { tag -> tag}) { tag ->
-                    ProductTag(tag)
+                Text(product?.title ?: "Unknown")
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Icon(Icons.Filled.Star, contentDescription = "ratings star")
+                    Text("%.1f".format(product?.rating ?: 0))
                 }
-            }
 
+                product?.tags?.takeIf { tags -> tags.isNotEmpty() }?.let { tags ->
+                    LazyRow(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+                        items(tags, key = { tag -> tag }) { tag ->
+                            ProductTag(tag.uppercase())
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
@@ -150,5 +185,13 @@ private fun ProductItemRow(
 @Preview
 @Composable
 private fun ProductListPreview() {
-    ProductList(products = listOf(Product(id = 1, title = "Wonderful skin cream", tags = listOf("Beauty", "Essence"))))
+    ProductList(
+        products = listOf(
+            Product(
+                id = 1,
+                title = "Wonderful skin cream",
+                tags = listOf("Beauty", "Essence")
+            )
+        )
+    )
 }
